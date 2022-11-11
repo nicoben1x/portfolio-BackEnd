9914970887cf1a21ebb2b1d4da5d752dfb29e818
nicoben1x <88980384+nicoben1x@users.noreply.github.com>
2022-11-11 15:35:30 -0300

    Se añadio los links de alternativa localhost o servidor web

--- src/main/java/com/ap/ap/ApApplication.java
+++ src/main/java/com/ap/ap/ApApplication.java
@@ -21,11 +21,14 @@ public class ApApplication {
 
 	}
 
+	// http://localhost:4200
+	// https://nicobenarg.web.app
+
 	@Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://nicobenarg.web.app"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
